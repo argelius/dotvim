e6e106a6054d5cf55e06fe513552a2b8504a8059
Andreas Argelius <andreas@reimu.(none)>
2013-04-10 21:07:02 +0200

    scriptage

--- linkify.sh
+++ linkify.sh
@@ -2,5 +2,9 @@
 
 USER_HOME=$(eval echo ~$USER)
 
+# Stash old vim files.
+mv ~/.vim ~/.vim_old
+mv ~/.vimrc ~/.vimrc_old
+
 ln -s `echo ${PWD}`/.vim `echo ${USER_HOME}`/.vim
 ln -s `echo ${PWD}`/.vimrc `echo ${USER_HOME}`/.vimrc
